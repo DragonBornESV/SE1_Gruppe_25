e4ccdedf1c82c6ea5a3e505ee019724162276922
DragonBornESV <32476705+DragonBornESV@users.noreply.github.com>
2019-10-29 11:04:16 +0100

    oprettede materialer

---
+++ worldofzuul/src/worldofzuul/Items.java
@@ -0,0 +1,20 @@
+package worldofzuul;
+
+public class Items {
+    Material[] materials = new Material[10];
+    
+    public Items(){
+        new Material("Iron",     3.80,  1);
+        new Material("Plastic" , 21.00, 1);
+        new Material("Aluminum", 8.10,  1);
+        new Material("Copper",   35.00, 1);
+        new Material("Rubber",   10.00, 1);
+        new Material("Paper",    17.50, 1);
+        new Material("Cloth",    35.00, 1);
+        new Material("Glass",    0.60,  1);
+        
+        
+                
+    }
+   
+}

fd1937a6bea8ef9ac06f8acc8f088d00d11ba094
DragonBornESV <32476705+DragonBornESV@users.noreply.github.com>
2019-11-06 21:13:02 +0100

    Alle inventorytabeller kan nu printes

---
+++ worldofzuul/src/worldofzuul/Item.java
@@ -0,0 +1,22 @@
+package worldofzuul;
+
+public class Item {
+    String name;
+    Material[] materials = new Material[10];
+    int[] materialCount = new int[10];
+    int count;
+    double weight;
+    int temp = 0; 
+
+    public Item(String name, Material[] materials, int[] materialCount, int count){
+        
+        this.name = name;
+        this.materials = materials;
+        this.materialCount = materialCount;
+        this.count = count;
+        for (int j = 0; j < materialCount.length; j++){
+             temp += materialCount[j];
+        }
+        weight = temp;
+    }
+}

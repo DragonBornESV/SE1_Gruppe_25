d7ac8468339d4e8faa9434efb29f9e7a7e482815
Villy <vivu19@student.sdu.dk>
2019-11-07 11:49:44 +0100

    Update Worldofzuul.java

--- worldofzuul/src/worldofzuul/Worldofzuul.java
+++ worldofzuul/src/worldofzuul/Worldofzuul.java
@@ -4,8 +4,12 @@ public class Worldofzuul {
     /* Startpunkt*/
     public static void main(String[] args) {
         Game run = new Game();
-        
-        run.play();
+        Parameter.createParameters();
+        Parameter.printScore();
+        Parameter.mapAddScore("City Equality", 10);
+        Parameter.mapAddScore("City Security", -10);
+        Parameter.printScore();
+        //run.play();
 
     }
     

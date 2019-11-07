e267dbee80308869e6b37b397f7b549f80118fc0
Andreas Edal Pedersen <anepe19@student.sdu.dk>
2019-11-07 15:21:10 +0100

    Fiksede main metoden
    - Villy ik' pil ved det!

--- worldofzuul/src/worldofzuul/Worldofzuul.java
+++ worldofzuul/src/worldofzuul/Worldofzuul.java
@@ -4,12 +4,8 @@ public class Worldofzuul {
     /* Startpunkt*/
     public static void main(String[] args) {
         Game run = new Game();
-        Parameter.createParameters();
-        Parameter.printScore();
-        Parameter.mapAddScore("City Equality", 10);
-        Parameter.mapAddScore("City Security", -10);
-        Parameter.printScore();
-        //run.play();
+
+        run.play();
 
     }
 }

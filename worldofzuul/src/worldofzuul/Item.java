407c4c50dd81a7c4d8e185cd2b56c50648fdfda1
Phillipvincent <55733174+Phillipvincent@users.noreply.github.com>
2019-11-12 16:36:06 +0100

    Update Item.java
    Added commentss

--- worldofzuul/src/worldofzuul/Item.java
+++ worldofzuul/src/worldofzuul/Item.java
@@ -1,12 +1,19 @@
 package worldofzuul;
-
+/*
+ * This class createss our objekt/datatype item.
+ * Item consists of a name in form of a string,
+ * a list of materials in form of a material array,
+ * the amount(count) of each materials in form of an integer array,
+ * the amount(count) of the item
+ * and the weight of the item in form of a double. 
+ */
 public class Item {
     String name;
     Material[] materials = new Material[10];
     int[] materialCount = new int[10];
     int count;
     double weight;
-    int temp = 0; 
+    int temp = 0;   // This is a temporary variable used to calculate the weight.
 
     public Item(String name, Material[] materials, int[] materialCount, int count){
         
@@ -14,6 +21,7 @@ public class Item {
         this.materials = materials;
         this.materialCount = materialCount;
         this.count = count;
+        // We take each value from the integer array materialCount and add them together one at a time. 
         for (int j = 0; j < materialCount.length; j++){
              temp += materialCount[j];
         }
